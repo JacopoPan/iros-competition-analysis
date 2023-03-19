4b32f41d6e9c753b98f009ffa1538821f3c63551
Jacopo Panerati <jacopo.panerati@utoronto.ca>
2023-03-19 17:58:15 +0400

    Reorganized data

--- parser.py
+++ parser.py
@@ -14,29 +14,94 @@ import bagpy
 from bagpy import bagreader
 import pickle
 import csv
+import os
+import re
+import matplotlib.pyplot as plt
+
 
 def run(
         test_arg,
     ):
-    bag = 'exp-data/arg1_2023-03-11-16-25-19.bag'
-    b = bagreader(bag)
-    print(b.topic_table) # get the list of topics
-    csvfiles = []
-    for t in b.topics:
-        data = b.message_by_topic(t)
-        csvfiles.append(data)
-    print(csvfiles[0])
+    # files = os.listdir('./exp-data/')
+    # print(files)
+    # for file in files:
+    #     if re.search('.bag', file): 
+    #         print(file)
+    #         b = bagreader('./exp-data/' + file)
+    #         print(b.topic_table) # get the list of topics
+    #         csvfiles = []
+    #         for t in b.topics:
+    #             data = b.message_by_topic(t)
+    #             csvfiles.append(data)
+    #         print(csvfiles[0])
 
-    values = 'sim-data/arg/save-flight-get_start-episode-1-03.16.2023_16.45.14/p0.csv'
-    with open(values) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for i, row in enumerate(csv_reader):
-            print (i, row)
+    for sol in ['arg', 'eku', 'h2']:
+        fig, axs = plt.subplots(3, 2)
 
-    pkl = 'exp-data/cmd_test_arg1.pkl'
-    with open(pkl, 'rb') as f:
-        data = pickle.load(f)
-    print(data)
+        for num in range(1,11):
+            with open('./exp/' + sol + str(num) + '.csv') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                time = []
+                x = []
+                y = []
+                z = []
+                for i, row in enumerate(csv_reader):
+                    if i == 0:
+                        continue
+                    elif i == 1:
+                        init_time = float(row[0])
+                    time.append(float(row[0])-init_time)
+                    x.append(float(row[5]))
+                    y.append(float(row[6]))
+                    z.append(float(row[7]))
+                axs[0, 0].plot(time, x, label='exp')
+                axs[1, 0].plot(time, y, label='exp')
+                axs[2, 0].plot(time, z, label='exp')
+
+            folder = './sim/' + sol + '/ep' + str(num)
+            with open(folder + '/x0.csv') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                time = []
+                x = []
+                for i, row in enumerate(csv_reader):
+                    time.append(float(row[0]))
+                    x.append(float(row[1]))
+            with open(folder + '/y0.csv') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                time = []
+                y = []
+                for i, row in enumerate(csv_reader):
+                    time.append(float(row[0]))
+                    y.append(float(row[1]))
+            with open(folder + '/z0.csv') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                time = []
+                z = []
+                for i, row in enumerate(csv_reader):
+                    time.append(float(row[0]))
+                    z.append(float(row[1]))
+            axs[0, 1].plot(time, x, label='sim')
+            axs[1, 1].plot(time, y, label='sim')
+            axs[2, 1].plot(time, z, label='sim')
+
+        fig.suptitle(sol)
+        axs[2, 0].set_xlabel('time')
+        axs[2, 1].set_xlabel('time')
+
+        axs[0, 0].set_ylabel('x')
+        axs[1, 0].set_ylabel('y')
+        axs[2, 0].set_ylabel('z')
+
+        axs[0, 0].set_title('real')
+        axs[0, 1].set_title('sim')
+        plt.show()
+
+    # for sol in ['arg', 'eku', 'h2']:
+    #     for num in range(1,11):
+    #         if type == 'exp':
+    #             file = './exp/' + sol + str(num) + '.csv'
+    #         else:
+    #             file = './' + type + '/' + sol + '/ep' + str(num) + '.csv'
 
 
 if __name__ == '__main__':
