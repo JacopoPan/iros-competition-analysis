6827c944412f792b6635c9c7556f3c8febfb6c86
Jacopo Panerati <jacopo.panerati@utoronto.ca>
2023-04-02 17:01:56 +0400

    simplified time series

--- parser.py
+++ parser.py
@@ -25,99 +25,115 @@ def run(
     for sol in ['arg', 'eku', 'h2']:
         fig, axs = plt.subplots(3, 4)
 
+        real_ref_time = [[] for i in range(10)]
+        real_ref_x = [[] for i in range(10)]
+        real_ref_y = [[] for i in range(10)]
+        real_ref_z = [[] for i in range(10)]
+
+        real_time = [[] for i in range(10)]
+        real_x = [[] for i in range(10)]
+        real_y = [[] for i in range(10)]
+        real_z = [[] for i in range(10)]
+        
+        sim_time = [[] for i in range(10)]
+
+        sim_ref_x = [[] for i in range(10)]
+        sim_ref_y = [[] for i in range(10)]
+        sim_ref_z = [[] for i in range(10)]
+
+        sim_x = [[] for i in range(10)]
+        sim_y = [[] for i in range(10)]
+        sim_z = [[] for i in range(10)]
+
         for num in range(1,11):
+            idx = num-1
 
             # real cmd
             first_cmd_time = None
             last_cmd_time = None
             with open('./exp/cmd_test_' + sol + str(num) + '.csv') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
-                real_ref_time = []
-                real_ref_x = []
-                real_ref_y = []
-                real_ref_z = []
                 for i, row in enumerate(csv_reader):
-                    if i == 0:
-                        continue
-                    elif i == 1:
+                    if i == 0: continue
+                    if i == 1:
                         real_ref_init_time = float(row[0])
                         first_cmd_time = real_ref_init_time
-                    real_ref_time.append(float(row[0])-real_ref_init_time)
-                    real_ref_x.append(float(row[2]))
-                    real_ref_y.append(float(row[3]))
-                    real_ref_z.append(float(row[4]))
-                last_cmd_time = real_ref_time[-1]+real_ref_init_time
-                axs[0, 0].plot(real_ref_time, real_ref_x, label='ref')
-                axs[1, 0].plot(real_ref_time, real_ref_y, label='ref')
-                axs[2, 0].plot(real_ref_time, real_ref_z, label='ref')
+                    real_ref_time[idx].append(float(row[0])-real_ref_init_time)
+                    real_ref_x[idx].append(float(row[2]))
+                    real_ref_y[idx].append(float(row[3]))
+                    real_ref_z[idx].append(float(row[4]))
+                last_cmd_time = real_ref_time[idx][-1]+real_ref_init_time
 
             # real vicon
             with open('./exp/' + sol + str(num) + '.csv') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
-                real_time = []
-                real_x = []
-                real_y = []
-                real_z = []
                 for i, row in enumerate(csv_reader):
-                    if i == 0:
-                        continue
-                    else:
-                        t = float(row[0])
-                        # if i == 1:
-                        #     init_time = t
-                        if t >= first_cmd_time and t <= last_cmd_time:
-                            real_time.append(t-real_ref_init_time)
-                            real_x.append(float(row[5]))
-                            real_y.append(float(row[6]))
-                            real_z.append(float(row[7]))
-                axs[0, 1].plot(real_time, real_x, label='exp')
-                axs[1, 1].plot(real_time, real_y, label='exp')
-                axs[2, 1].plot(real_time, real_z, label='exp')
+                    if i == 0: continue
+                    t = float(row[0])
+                    if t >= first_cmd_time and t <= last_cmd_time:
+                        real_time[idx].append(t-real_ref_init_time)
+                        real_x[idx].append(float(row[5]))
+                        real_y[idx].append(float(row[6]))
+                        real_z[idx].append(float(row[7]))
 
             # sim cmd
+            sim_ref_time = []
             with open('./sim/' + sol + '/episode' + str(num) + '.csv') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
-                sim_ref_time = []
-                sim_ref_x = []
-                sim_ref_y = []
-                sim_ref_z = []
                 for i, row in enumerate(csv_reader):
-                    if i == 0:
-                        continue
+                    if i == 0: continue
                     sim_ref_time.append(float(row[0]))
-                    sim_ref_x.append(float(row[2]))
-                    sim_ref_y.append(float(row[3]))
-                    sim_ref_z.append(float(row[4]))
-                axs[0, 2].plot(sim_ref_time, sim_ref_x, label='ref')
-                axs[1, 2].plot(sim_ref_time, sim_ref_y, label='ref')
-                axs[2, 2].plot(sim_ref_time, sim_ref_z, label='ref')
+                    sim_ref_x[idx].append(float(row[2]))
+                    sim_ref_y[idx].append(float(row[3]))
+                    sim_ref_z[idx].append(float(row[4]))
 
             # sim pos
             folder = './sim/' + sol + '/ep' + str(num)
+            sim_x_time = []
+            sim_y_time = []
+            sim_z_time = []
             with open(folder + '/x0.csv') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
-                sim_x_time = []
-                sim_x = []
                 for i, row in enumerate(csv_reader):
                     sim_x_time.append(float(row[0]))
-                    sim_x.append(float(row[1]))
+                    sim_x[idx].append(float(row[1]))
             with open(folder + '/y0.csv') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
-                sim_y_time = []
-                sim_y = []
                 for i, row in enumerate(csv_reader):
                     sim_y_time.append(float(row[0]))
-                    sim_y.append(float(row[1]))
+                    sim_y[idx].append(float(row[1]))
             with open(folder + '/z0.csv') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
-                sim_z_time = []
-                sim_z = []
                 for i, row in enumerate(csv_reader):
                     sim_z_time.append(float(row[0]))
-                    sim_z.append(float(row[1]))
-            axs[0, 3].plot(sim_x_time, sim_x, label='sim')
-            axs[1, 3].plot(sim_y_time, sim_y, label='sim')
-            axs[2, 3].plot(sim_z_time, sim_z, label='sim')
+                    sim_z[idx].append(float(row[1]))
+            if sim_x_time != sim_y_time or sim_x_time != sim_z_time or sim_x_time != sim_ref_time:
+                print('Error: time mismatch')
+                exit()
+            sim_time[idx] = sim_x_time
+
+            # comparing time sampling
+            print(sim_time[idx][0], sim_time[idx][-1], len(sim_time[idx]))
+            print(real_ref_time[idx][0], real_ref_time[idx][-1], len(real_ref_time[idx]))
+            print(real_time[idx][0], real_time[idx][-1], len(real_time[idx]))
+            print()
+
+            # plot one-by-one
+            axs[0, 0].plot(real_ref_time[idx], real_ref_x[idx], label='ref')
+            axs[1, 0].plot(real_ref_time[idx], real_ref_y[idx], label='ref')
+            axs[2, 0].plot(real_ref_time[idx], real_ref_z[idx], label='ref')
+
+            axs[0, 1].plot(real_time[idx], real_x[idx], label='exp')
+            axs[1, 1].plot(real_time[idx], real_y[idx], label='exp')
+            axs[2, 1].plot(real_time[idx], real_z[idx], label='exp')
+
+            axs[0, 2].plot(sim_time[idx], sim_ref_x[idx], label='ref')
+            axs[1, 2].plot(sim_time[idx], sim_ref_y[idx], label='ref')
+            axs[2, 2].plot(sim_time[idx], sim_ref_z[idx], label='ref')  
+
+            axs[0, 3].plot(sim_time[idx], sim_x[idx], label='sim')
+            axs[1, 3].plot(sim_time[idx], sim_y[idx], label='sim')
+            axs[2, 3].plot(sim_time[idx], sim_z[idx], label='sim')
 
         # labels
         fig.suptitle(sol)
@@ -135,6 +151,7 @@ def run(
         axs[0, 2].set_title('sim_cmd')
         axs[0, 3].set_title('sim')
 
+        # axes limits
         axs[0, 0].set_ylim(-3.5,3.5)
         axs[0, 1].set_ylim(-3.5,3.5)
         axs[0, 2].set_ylim(-3.5,3.5)
